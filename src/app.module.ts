5bb270b9cb1a6f04a38b1af4f66690f6909b104d
aymeennefzi <aymennefzi67@gmail.com>
2024-02-24 12:46:16 +0100

    Add configuration

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,12 +3,13 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import {DevtoolsModule} from "@nestjs/devtools-integration";
 import * as process from "process";
+import {MongooseModule} from "@nestjs/mongoose";
 
 @Module({
   imports: [
       DevtoolsModule.register({
         http : process.env.NODE_ENV !=='production' ,
-      })
+      }), MongooseModule.forRoot('mongodb://127.0.0.1/HRmanagment')
   ],
   controllers: [AppController],
   providers: [AppService],
