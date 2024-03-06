5ae10cc0ecfd96fec94f2e4e7c41e9a7de9d7229
aymeennefzi <aymennefzi67@gmail.com>
2024-03-06 23:06:16 +0100

    fixed issues (jwtService AuthService)

--- src/app.module.ts
+++ src/app.module.ts
@@ -5,6 +5,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { BookModule } from './book/book.module';
 import { AuthModule } from './auth/auth.module';
+import {CongesModule} from "./conges/conges.module";
 
 @Module({
   imports: [
@@ -15,6 +16,8 @@ import { AuthModule } from './auth/auth.module';
     MongooseModule.forRoot(process.env.DB_URI),
     BookModule,
     AuthModule,
+      CongesModule,
+
   ],
   controllers: [AppController],
   providers: [AppService],
--- src/conges/conges.module.ts
+++ src/conges/conges.module.ts
@@ -6,18 +6,20 @@ import {User, UserSchema} from "../auth/Shemas/User.shema";
 import {Leave, LeaveSchema} from "./Schema/Leaves.schema";
 import {AuthModule} from "../auth/auth.module";
 import {AuthService} from "../auth/auth.service";
+import {JwtService} from "@nestjs/jwt";
+import {MailerService} from "../auth/Mail.service";
 
 
 @Module({
   imports: [
     MongooseModule.forFeature([
       { name: Leave.name, schema: LeaveSchema },
-      {name : User.name , schema : UserSchema}
+      {name : User.name , schema : UserSchema},
     ]),
       AuthModule
   ],
   controllers :[CongeController],
-  providers: [ CongeService , AuthService ],
+  providers: [ CongeService , AuthService , JwtService , MailerService],
 
 })
 export class CongesModule {}
